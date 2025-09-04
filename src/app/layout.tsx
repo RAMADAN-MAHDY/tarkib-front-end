19827dbe2682bae00338e10283fc0393494987d6
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-09-04 12:19:42 +0300

    feat: Enhance SEO and metadata in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,9 +43,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
+        <head>
+                <meta name="google-site-verification" content="6NkDuQUFW4FrvJrL8w7hefEj9Oban0KKQDSuSYOHwpc" />
+                <meta name="author" content="شركة تراكيب للاثاث والديكورات" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="robots" content="index, follow" />
+                <meta name="geo.region" content="SA" />
+                <meta name="geo.placename" content=" تراكيب للاثاث والديكورات " />
+                <meta name="theme-color" content="#059669" />
+                <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/image_tarkib/WhatsApp Image 2025-09-03 at 15.46.00_335e941d.jpg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                {/* <link rel="preload" href="/animation/EbAMuFGCsN.lottie" as="fetch" type="application/json" crossOrigin="anonymous" /> */}
+
+                {/* Structured Data (JSON-LD) */}
+                <script type="application/ld+json" dangerouslySetInnerHTML={{
+                    __html: `
+{
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "شركة تراكيب للاثاث والديكورات",
+  "image": "/image_tarkib/WhatsApp Image 2025-09-03 at 15.46.00_335e941d.jpg",
+  "url": "https://nazafaa.com",
+  "telephone": "0568761334",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "الخرج",
+    "addressRegion": "الرياض",
+    "addressCountry": "SA"
+  },
+  "description": "شركة تنظيف فلل وشقق ومكاتب بالخرج والرياض، مكافحة الحشرات، أسعار مناسبة وجودة عالية.",
+  "areaServed": ["الخرج", "الرياض"],
+  "priceRange": "$$",
+  "openingHours": "Su-Sa 08:00-22:00"
+}
+        `}} />
+            </head>
       <body
         className={`${cairo.variable} antialiased bg-white text-gray-900`}
         style={{fontFamily: 'inherit'}}
