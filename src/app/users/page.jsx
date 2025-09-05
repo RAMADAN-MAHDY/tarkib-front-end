7c1481dc6cedc8f5cacea6d6cf159141cd53ec70
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-09-05 16:06:23 +0300

    feat: Update AdminVerify component to accept onVerify prop and modify UsersPage to handle verification state

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,14 +1,21 @@
-
-import UsersList from "../components/admin/UsersList";
+"use client";
+import UsersList from '../components/admin/UsersList';
 import ClickCountViewer from '../components/admin/ClickCountViewer';
 import AdminVerify from '../components/admin/AdminVerify';
 
+
+import { useState } from 'react';
+
 export default function UsersPage() {
+  const [isVerified, setIsVerified] = useState(false);
+
   return (
     <>
-      <AdminVerify />
-      <ClickCountViewer />
-      <UsersList />
+      <AdminVerify onVerify={setIsVerified} />
+      {isVerified && <>
+        <ClickCountViewer />
+        <UsersList />
+      </>}
     </>
   );
 }
